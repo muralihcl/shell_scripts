9566d56cd08a4a4538b8871ed1d350ae5b4e9d8d
Muralidhara Kakkunje <user@example.com>
2024-03-11 21:10:45 +0530

    Removing debug

--- case.sh
+++ case.sh
@@ -6,7 +6,7 @@
 # Description   : The script is a demonstration of case over nested if elif else logic
 
 # Enable debug
-set -x
+# set -x
 
 # Define variables
 os=$(uname)
@@ -64,4 +64,4 @@ case $number_of_files_in_directory in
       ;;
 esac
 
-echo "There are $number_of_files_in_directory in $PWD directory"
+echo "There are $number_of_files_in_directory files in $PWD directory"
