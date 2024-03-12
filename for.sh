b7da61a9ca825029a4454b2c8f033b893f363495
Muralidhara Kakkunje <user@example.com>
2024-03-12 20:59:55 +0530

    Including for.sh

---
+++ for.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Author        : Muralidhara Kakkunje
+# Version       : 1.0
+# Date          : 12-Mar-2024
+# Description   : The script is a demonstration of for loop
+
+for number in {1..10}
+do
+    echo "The number is $number"
+done
+
+for file in `ls`
+do
+    echo "The file name is $file"
+done
