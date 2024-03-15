db24b95e2e1bb81a0b3f3c6af01a44a6540dc0f8
Muralidhara Kakkunje <user@example.com>
2024-03-15 20:46:02 +0530

    Updating the user manage script

--- user_manage.sh
+++ user_manage.sh
@@ -22,7 +22,7 @@ function create_user() {
     user_name=$1
     if [[ ! -z $2 ]] && [[ $2 != '' ]]
     then
-        comment=$(echo $2 | sed 's/ /_/')
+        comment=$2
     else
         comment="User $user_name"
     fi
@@ -33,7 +33,7 @@ function create_user() {
         user_shell=/bin/bash
     fi
     echo "Creating the user with provided details"
-    useradd -c "\$comment" -s $user_shell -m ${user_name}
+    useradd -c "$comment" -s $user_shell -m ${user_name}
     if [[ $? -eq 0 ]]
     then
         echo "Successfully created the user $user_name"
@@ -48,9 +48,9 @@ function modify_user() {
     user_name=$1
     if [[ ! -z $2 ]] && [[ $2 != '' ]]
     then
-        comment=$(echo $2 | sed 's/ /_/')
+        comment=$2
     else
-        comment="User_${user_name}"
+        comment="User ${user_name}"
     fi
     if [[ ! -z $3 ]] && [[ $3 != '' ]]
     then
