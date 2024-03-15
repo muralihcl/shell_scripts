7feba665da182156d3d50c2c37602e65cff42156
divyap2808 <163121007+divyap2808@users.noreply.github.com>
2024-03-15 16:17:45 +0530

    Update case.sh

--- case.sh
+++ case.sh
@@ -5,26 +5,7 @@
 # Date          : 15-Mar-2024
 # Description   : The script is a demonstration of case over nested if elif else logic
 
-# Enable debug
-# set -x
 
-# Define variables
-os=$(uname)
-
-case $os in
-  'SunOS')
-      echo "[Case] You are working on a Solaris oprating system"
-      ;;
-  'HP-UX')
-      echo "[Case] You are working on a HP-UX oprating system"
-      ;;
-  'Linux')
-      echo "[Case] You are working on a Linux oprating system"
-      ;;
-  '*')
-      echo "[Case] I don't know on which operating system you are working on"
-      ;;
-esac
 
 number_of_files_in_directory=$(ls -1 | wc -l)
 
