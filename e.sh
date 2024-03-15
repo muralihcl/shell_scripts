439523ce5a31c0ef9092bca9117944fe5905aa1f
divyap2808 <163121007+divyap2808@users.noreply.github.com>
2024-03-15 16:13:14 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Author        : Muralidhara Kakkunje
+# Author        : DIVYA
 # Version       : 1.0
-# Date          : 11-Mar-2024
+# Date          : 15-Mar-2024
 # Description   : The script is a demonstration of case over nested if elif else logic
 
 # Enable debug
