5e1d15c85fbde35fc4fa54adf5d2caede2cc3fb2
divyap2808 <163121007+divyap2808@users.noreply.github.com>
2024-03-18 15:39:42 +0530

    Create g.sh

---
+++ g.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Author        : DDDD
+# Version       : 1.0
+# Date          : 18-Mar-2024
+# Description   : The script is a demonstration of for loop
+
+shopt -s nullglob
+
+for number in {1..10}
+do
+    echo "The number is $number"
+done
+
+# The for loop breaks with file names with spaces
+for file in $(ls)
+do
+    echo "The file name is $file"
+done
+
+# The glob way of approach works fine even with file names with spaces
+for file in *
+do
+    echo "The file name is $file"
+done
