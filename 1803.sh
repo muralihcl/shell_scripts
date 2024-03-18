4382b85594eb3cc6ce8d5ac2385893b9e4c0253e
Shruthi <Shruthidinesh@gmail.com>
2024-03-18 20:17:37 +0530

    new file

---
+++ 1803.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Author        : Muralidhara Kakkunje
+# Version       : 1.0
+# Date          : 28-Feb-2024
+# Description   : The script is a demonstration of greet functionality
+
+# Define variables from accepting the inputs
+echo -n "Enter your name: "
+read name
+
+echo -n "enter your city: "
+read city
+
+echo "hi $name and you live in $city"
+
+
+
